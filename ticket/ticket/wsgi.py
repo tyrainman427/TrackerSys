da0746e906f4a351e397d213594b5f74be43c1fa
tyrainman427 <tyraineytech@gmail.com>
2020-03-17 18:44:45 -0400

    made changes

--- ticket/ticket/wsgi.py
+++ ticket/ticket/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticket.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticket.settings.dev')
 
 application = get_wsgi_application()
