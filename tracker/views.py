311c3f9efd01ee64f466729cffba7cebc0daa79c
tyrainman427 <tyraineytech@gmail.com>
2020-05-29 03:51:36 -0400

    fixed bug crashes

--- tracker/views.py
+++ tracker/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Ticket, TicketUser
 from .forms import TicketForm
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required,login_required
 import csv, io
 # from braces.views import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
@@ -16,6 +16,7 @@ from django.views.generic import DetailView, ListView, UpdateView, CreateView, D
 
 # Create your views here.
 
+@login_required
 def dashboard(request):
     tickets = Ticket.objects.all()
     users = User.objects.all()
