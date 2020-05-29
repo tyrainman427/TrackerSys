8e86018c68f427f8c30d2baca78c8915c436fb09
tyrainman427 <tyraineytech@gmail.com>
2020-05-29 02:55:02 -0400

    pushing

--- ticket/urls.py
+++ ticket/urls.py
@@ -7,7 +7,7 @@ from users import views as user_views
 
 
 urlpatterns = [
-    path('tracker/', include('tracker.urls')),
+    path('', include('tracker.urls')),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
